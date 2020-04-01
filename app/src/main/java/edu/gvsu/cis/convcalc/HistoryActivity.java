5a6b04a170fa55a5193fb0723cce3b6c20727481
Nelskika <55363532+Nelskika@users.noreply.github.com>
2020-03-31 21:10:40 -0400

    Initial commit

---
+++ app/src/main/java/edu/gvsu/cis/convcalc/HistoryActivity.java
@@ -0,0 +1,36 @@
+package edu.gvsu.cis.convcalc;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import edu.gvsu.cis.convcalc.dummy.HistoryContent;
+
+public class HistoryActivity extends AppCompatActivity implements HistoryFragment.OnListFragmentInteractionListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+    @Override
+
+    public void onListFragmentInteraction(HistoryContent.HistoryItem item) {
+        System.out.println("Interact!");
+        Intent intent = new Intent();
+        String[] vals = {item.fromVal.toString(), item.toVal.toString(), item.mode, item.fromUnits, item.toUnits};
+        intent.putExtra("item", vals);
+        setResult(MainActivity.HISTORY_RESULT,intent);
+        finish();
+    }
+}
